3d9bb127390d51eb28b5f478f3125d32466aeeb6
xuyinan <xuyinan228@163.com>
2025-05-02 11:08:09 +0800

    init hello-world

---
+++ coreJava-01-hello/src/main/java/com/atguigu/hello/HelloWorld.java
@@ -0,0 +1,16 @@
+package com.atguigu.hello;
+
+/**
+ * @author Xu.Yinan
+ * 这是一个文档注释，它和多行注释的差别是能被javadoc.exe进行文档化
+ *
+ * 这是我的一个HelloWorld程序
+ */
+public class HelloWorld {
+
+
+    public static void main(String[] args) {
+        // 这是一个单行注释
+        System.out.println("Hello World");
+    }
+}
