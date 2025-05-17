caf51ca5f9a656d1509a138a706dbba874033aa6
xuyinan <xuyinan228@163.com>
2025-05-17 23:43:25 +0800

    update oop

--- coreJava-05-oop/src/main/java/com/atguigu/oop/advance/polymorphism/PolymorphismTest.java
+++ coreJava-05-oop/src/main/java/com/atguigu/oop/advance/polymorphism/PolymorphismTest.java
@@ -1,14 +1,18 @@
 package com.atguigu.oop.advance.polymorphism;
 
+import java.util.Random;
+
 public class PolymorphismTest {
     public static void main(String[] args) {
-        Person p1 = new WoMan();
+        int randomNum = new Random().nextInt(100);
+
+        Person p1 = randomNum > 50 ? new WoMan() : new Man();
         p1.eat();
         p1.walk();
 
         if( p1 instanceof Man ){
             ((Man) p1).earnMoney();
-        } else if( p1 instanceof WoMan){
+        } else {
             ((WoMan) p1).shopping();
         }
     }
